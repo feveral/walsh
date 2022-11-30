aa886f587b83fe7d0ee2cd1f968a140335fef649
楊宗翰 <feveraly@gmail.com>
2022-11-30 20:55:13 +0800

    feat: add spinner

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,14 +7,17 @@ import { Item } from "../models/Item";
 import { MemeItem } from "../models/MemeItem";
 import Dialog from "../components/Dialog";
 import ReactJson from 'react-json-view'
+import Spinner from "../components/Spinner";
 
 export default function MainPage () {
 
     const [items, setItems] = useState<Item[]>([]);
     const [dialogOpen, setDialogOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState<Item>();
+    const [isLoading, setIsLoading] = useState(false);
 
     const fetchItems = async (urlString: string) => {
+        setIsLoading(true);
         let url = new URL(urlString);
         const response = await axios.get(urlString);
 
@@ -27,6 +30,7 @@ export default function MainPage () {
                 response.data.items.map((json: any) => new CustomSearchItem(json))
             )
         }
+        setIsLoading(false);
     }
 
     return (
@@ -35,27 +39,31 @@ export default function MainPage () {
                 onSearch = { (text) => { fetchItems(text) } }
                 placeholder = 'Type API URL ...'
             />
-            <div className="flex flex-wrap justify-center mt-5">
-                {
-                    items.map((item: any, index) => {
-                        return <div key={index} className="flex-4 mx-3">
-                            <ItemCell 
-                                title={item.getTitle()}
-                                imageUrl={item.getImageUrl()}
-                                onClick={ () => {
-                                    setSelectedItem(item)
-                                    setDialogOpen(true)
-                                }}
-                            />
-                        </div>
-                    })
-                }
-            </div>
-            <div>
-                <Dialog open={dialogOpen} setOpen={setDialogOpen}>
-                    <ReactJson src={selectedItem?.getJson() as object} />
-                </Dialog>
-            </div>
+            {
+                isLoading
+                ?   <div className="flex justify-center mt-40">
+                        <Spinner></Spinner>
+                    </div>
+                :   <div className="flex flex-wrap justify-center mt-5">
+                    {
+                        items.map((item: any, index) => {
+                            return <div key={index} className="flex-4 mx-3">
+                                <ItemCell 
+                                    title={item.getTitle()}
+                                    imageUrl={item.getImageUrl()}
+                                    onClick={ () => {
+                                        setSelectedItem(item)
+                                        setDialogOpen(true)
+                                    }}
+                                />
+                            </div>
+                        })
+                    }
+                    </div>
+            }
+            <Dialog open={dialogOpen} setOpen={setDialogOpen}>
+                <ReactJson src={selectedItem?.getJson() as object} />
+            </Dialog>
         </div>
     );
 }
